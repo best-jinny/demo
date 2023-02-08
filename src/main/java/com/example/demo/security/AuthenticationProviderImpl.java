d9dc1e8bd2fae35a944b880293dd3e0574b44071
hjp <hj.park@renocomms.com>
2023-02-08 17:29:22 +0900

    update

--- src/main/java/com/example/demo/security/AuthenticationProviderImpl.java
+++ src/main/java/com/example/demo/security/AuthenticationProviderImpl.java
@@ -1,41 +1,48 @@
-//package com.example.demo.security;
-//
-//import com.example.demo.service.CustomUserDetailsService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.stereotype.Component;
-//
-///*
-// * AuthenticationManager 하위 실제 인증 처리하는 AuthenticationProvider 구현
-// */
-//
-//
-//@Component
-//@RequiredArgsConstructor
-//public class AuthenticationProviderImpl implements AuthenticationProvider {
-//
-//    private final CustomUserDetailsService customUserDetailsService;
-//
-//    // CustomAuthenticationFilter 에서 생성된 UsernamePasswordAuthenticationToken 으로 해당 회원 DB 조회하여 인증
-//    @Override
-//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-//        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-//
-//        String username = token.getName();
-//        String password = (String) token.getCredentials();
-//
-//        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-//
-//
-//    }
-//
-//    @Override
-//    public boolean supports(Class<?> authentication) {
-//        return false;
-//    }
-//}
+package com.example.demo.security;
+
+import com.example.demo.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/*
+ * AuthenticationManager 하위 실제 인증 처리하는 AuthenticationProvider 구현
+ */
+
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationProviderImpl implements AuthenticationProvider {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    // CustomAuthenticationFilter 에서 생성된 UsernamePasswordAuthenticationToken 으로 해당 회원 DB 조회하여 인증
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+
+        String username = token.getName();
+        String password = (String) token.getCredentials();
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword()))
+            throw new BadCredentialsException(userDetails.getUsername() + "Invalid password");
+
+
+        return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), token, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
